989d99a1d0d3852de2eb3669faa14ededa1128b5
Olusegun <olusegunbamgbelu755@gmail.com>
2026-07-16 04:51:53 +0100

    feat:hero section done

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,10 +2,14 @@ import HeroLogo from "../assets/images/logo-full.svg?react";
 
 export default function Hero() {
   return (
-    <section>
-      <HeroLogo />
-      <h1 className="capitalize">Your journey to coding conf 2026 starts here</h1>
-      <p>Secure your spot at next's year biggest coding conference.</p>
+    <section className="flex flex-col items-center ">
+      <HeroLogo className="w-43 h-auto mt-10 mb-8 md:w-48" />
+      <div className="w-full max-w-100 text-center flex flex-col gap-5 md:gap-7 md:max-w-180 lg:max-w-220 p-3">
+        <h1 className="capitalize text-3xl font-bold md:text-5xl lg:text-6xl">
+          Your journey to coding conf 2026 starts here!
+        </h1>
+        <p>Secure your spot at next's year biggest coding conference.</p>
+      </div>
     </section>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import svgr from 'vite-plugin-svgr'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        dimensions: false,
+      },
+    }),
+  ],
+});
